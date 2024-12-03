4462d2275da9ed935ccc45e88b31cff1a7530245
Beatrix Zselezny <cattybaby75@gmail.com>
2024-12-03 20:38:45 +0100

    I was committed the sources in locations directory

--- locations/Bar.java
+++ locations/Bar.java
@@ -1,27 +1,32 @@
 package locations;
-
 import victims.Victim;
 import java.util.ArrayList;
 import java.util.List;
 
+
+// Class representing a Bar location with a list of victims
 public class Bar {
     private String name;
     private List<Victim> victims;
 
+    // Constructor to initialize a Bar object with a name and list of victims
     public Bar(String name, List<Victim> victims) {
         this.name = name;
         this.victims = victims;
     }
 
+    // Default constructor to initialize a Bar object with a default name and an empty list of victims public Bar() {
     public Bar() {
         this.name = "Default Bar Name";
         this.victims = new ArrayList<>();
     }
 
+    // Method to get the name of the bar
     public String getName() {
         return name;
     }
 
+    // Method to find a victim based on gender, age, and hair color
     public Victim findVictim(String gender, int age, String hairColor) {
         for (Victim victim : victims) {
             if (victim.getGender().equals(gender) && victim.getAge() == age && victim.getHairColor().equals(hairColor)) {
@@ -31,4 +36,4 @@ public class Bar {
         return null;
     }
 }
-  
+
--- locations/DrugDen.java
+++ locations/DrugDen.java
@@ -1,6 +1,6 @@
+package locations;
 
-  package locations;
-
+  // Class representing a Drug Den location
   public class DrugDen {
-  
+  // You can add properties and methods relevant to the Drug Den here
   }
