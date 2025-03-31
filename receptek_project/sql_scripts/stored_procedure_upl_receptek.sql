f25d83753a16b49587a124b20c39f1a012c78075
Beatrix Zselezny <cattybaby75@gmail.com>
2025-03-31 12:13:54 +0200

    I was fix it the RISE NOTTICE functions

--- receptek_project/sql_scripts/stored_procedure_upl_receptek.sql
+++ receptek_project/sql_scripts/stored_procedure_upl_receptek.sql
@@ -1,29 +1,29 @@
-  CREATE OR REPLACE FUNCTION  InteractiveRecipeUploader(\
-      receptid INT,\
-      recept_név VARCHAR DEFAULT 'Nincs megadva',\
-      elkészítés VARCHAR DEFAULT 'Nincs megadva',\
-      jegyzet VARCHAR DEFAULT 'Nincs megadva',\
-      recept_osztály_id INT,\
-      alaplé VARCHAR DEFAULT 'Nincs megadva',\
-      zsíradék VARCHAR DEFAULT 'Nincs megadva',\
-      hús VARCHAR DEFAULT 'Nincs megadva',\
-      zöldség VARCHAR DEFAULT 'Nincs megadva',\
-      tejtermék VARCHAR DEFAULT 'Nincs megadva',\
-      gabonaféle VARCHAR DEFAULT 'Nincs megadva',\
-      szárazáru VARCHAR DEFAULT 'Nincs megadva',\
-      állati_termék VARCHAR DEFAULT 'Nincs megadva',\
-      gyümölcs VARCHAR DEFAULT 'Nincs megadva',\
-      fűszer VARCHAR DEFAULT 'Nincs megadva',\
-      magvak VARCHAR DEFAULT 'Nincs megadva',\
-      feldolgozott_élelmiszer VARCHAR DEFAULT 'Nincs megadva',\
-      pékáru VARCHAR DEFAULT 'Nincs megadva',\
-      ital VARCHAR DEFAULT 'Nincs megadva',\
-      konzervtermék VARCHAR DEFAULT 'Nincs megadva',\
-      édességek VARCHAR DEFAULT 'Nincs megadva',\
-      kész_szósz VARCHAR DEFAULT 'Nincs megadva',\
-      fűszernövény VARCHAR DEFAULT 'Nincs megadva',\
-      kész_krém VARCHAR DEFAULT 'Nincs megadva'\
-) RETURNS VOID AS $$\
+  CREATE OR REPLACE FUNCTION  InteractiveRecipeUploader(
+      receptid INT,
+      recept_név VARCHAR DEFAULT 'Nincs megadva',
+      elkészítés VARCHAR DEFAULT 'Nincs megadva',
+      jegyzet VARCHAR DEFAULT 'Nincs megadva',
+      recept_osztály_id INT,
+      alaplé VARCHAR DEFAULT 'Nincs megadva',
+      zsíradék VARCHAR DEFAULT 'Nincs megadva',
+      hús VARCHAR DEFAULT 'Nincs megadva',
+      zöldség VARCHAR DEFAULT 'Nincs megadva',
+      tejtermék VARCHAR DEFAULT 'Nincs megadva',
+      gabonaféle VARCHAR DEFAULT 'Nincs megadva',
+      szárazáru VARCHAR DEFAULT 'Nincs megadva',
+      állati_termék VARCHAR DEFAULT 'Nincs megadva',
+      gyümölcs VARCHAR DEFAULT 'Nincs megadva',
+      fűszer VARCHAR DEFAULT 'Nincs megadva',
+      magvak VARCHAR DEFAULT 'Nincs megadva',
+      feldolgozott_élelmiszer VARCHAR DEFAULT 'Nincs megadva',
+      pékáru VARCHAR DEFAULT 'Nincs megadva',
+      ital VARCHAR DEFAULT 'Nincs megadva',
+      konzervtermék VARCHAR DEFAULT 'Nincs megadva',
+      édességek VARCHAR DEFAULT 'Nincs megadva',
+      kész_szósz VARCHAR DEFAULT 'Nincs megadva',
+      fűszernövény VARCHAR DEFAULT 'Nincs megadva',
+      kész_krém VARCHAR DEFAULT 'Nincs megadva'
+) RETURNS VOID AS $$
  DECLARE
     v.alaplé VARCHAR := CASE WHEN $6 IS NULL OR $6  = '' THEN NULL ELSE $6 END;
     v.zsíradék VARCHAR := CASE WHEN $7 IS NULL OR $7 = '' THEN NULL ELSE $7 END;
@@ -44,20 +44,22 @@
     v.kész_szósz VARCHAR := CASE WHEN $22 IS NULL OR $22 = '' THEN NULL ELSE $22 END;
     v.fűszernövény VARCHAR := CASE WHEN $23 IS NULL OR $23 = '' THEN NULL ELSE $23 END;
     v.kész_krém VARCHAR := CASE WHEN $24 IS NULL OR $24 = '' THEN NULL ELSE $24 END;
- BEGIN\
- -- Ellenőrizzük, hogy már létezik -e a megadott recept_id\
-     IF NOT EXISTS (SELECT 1 FROM receptek WHERE receptek.receptid = $1)  THEN\
- -- Hem már létezik, beszúrunk egy új rekordot\
-        INSERT INTO receptek (receptid, recept_név, elkészítés, jegyzet, recept_osztály_id, alaplé, zsíradék, hús, zöldség, tejtermék,\
- gabonaféle, szárazáru, állati_termék, gyümölcs, fűszer, magvak, feldolgozott_élelmiszer, pékáru, ital, konzervtermék, édességek,\
- kész_szósz, fűszernövény, kész_krém)\
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24);\
-        RAISE NOTICE 'A rekord sikeresen létrejött: receptid = %, recept_név = %', $1, $2;\
-            EXCEPTION WHEN others THEN\
-              RAISE NOTICE 'Hiba történt a beszúrás során. Nem sikerült a művelet!';
-          END;
-     ELSE\
- -- Ha már létezik, értesítést küldünk RAISE NOTICE 'A megadott receptid (%) már létezik!', receptid;\
-     END IF;\
-  END;\
+ BEGIN
+       -- Ellenőrizzük, hogy már létezik -e a megadott recept_id
+    IF NOT EXISTS (SELECT 1 FROM receptek WHERE receptek.receptid = $1)  THEN
+       -- Hem már létezik, beszúrunk egy új rekordot
+        INSERT INTO receptek (receptid, recept_név, elkészítés, jegyzet, recept_osztály_id, alaplé, zsíradék, hús, zöldség, tejtermék,
+ gabonaféle, szárazáru, állati_termék, gyümölcs, fűszer, magvak, feldolgozott_élelmiszer, pékáru, ital, konzervtermék, édességek,
+ kész_szósz, fűszernövény, kész_krém)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24);
+
+
+        RAISE NOTICE 'A rekord sikeresen létrejött: receptid = %, recept_név = %', $1, $2;
+    END IF;
+ EXCEPTION
+    WHEN OTHERS THEN
+        -- Hibakezelés
+        RAISE NOTICE 'Hiba történt a beszúrás során. Nem sikerült a művelet!';
+        RAISE EXCEPTION 'Nem várt hiba fordult elő!';
+  END;
  $$ LANGUAGE plpgsql;
