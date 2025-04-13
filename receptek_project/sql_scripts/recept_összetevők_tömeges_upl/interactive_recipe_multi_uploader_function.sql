f0253f8af20c5ca5b8d3335a3a89d29d35aebc0e
Beatrix Zselezny <cattybaby75@gmail.com>
2025-04-13 02:18:46 +0200

    I was fix it my multi_uploader function with intaractive script

--- receptek_project/sql_scripts/recept_összetevők_tömeges_upl/interactive_recipe_multi_uploader_function.sql
+++ receptek_project/sql_scripts/recept_összetevők_tömeges_upl/interactive_recipe_multi_uploader_function.sql
@@ -1,34 +1,38 @@
-CREATE OR REPLACE FUNCTION InteractiveRecipeMultiUploader.sql(
-    receptid INT,
-    recept_sorszám INT,
-    összetevő_id INT,
-    mennyiség INT,
-    mérték_mennyiség_id INT,
-    összetevő_osztály_id INT,
-    recept_osztály_id INT
+CREATE OR REPLACE FUNCTION InteractiveRecipeMultiUploader(
+    receptid TEXT,
+    recept_sorszám TEXT,
+    összetevő_id TEXT,
+    mennyiség TEXT,
+    mérték_mennyiség_id TEXT,
+    összetevő_osztály_id TEXT,
+    recept_osztály_id TEXT
 ) RETURNS VOID AS $$
 BEGIN
-  -- Ellenőrizzük, hogy hogy már létezik -e, a megadott recept ID:
-    IF EXISTS (SELECT 1, 2, 3, 4 FROM recept_összetevők WHERE recept_összetevők.receptid = $1, $2, $3, $4) THEN
-        	RAISE NOTICE 'Ez az ID már foglalt! Kérlek, válassz másikat!';
-	ELSE
-  -- Beszúrjuk az új rekordokat:
+    -- Ellenőrizzük a tömbhosszokat
+    IF array_length(string_to_array(replace(receptid, ' ', ''), ','), 1) IS DISTINCT FROM array_length(string_to_array(replace(recept_sorszám, ' ', ''), ','), 1) THEN
+        RAISE EXCEPTION 'Hiba: Az oszlopok tömbjeinek hosszúsága nem egyezik!';
+    END IF;
+
+    -- Konvertálás és beszúrás
     WITH data AS (
-         SELECT UNNEST(string_to_array(:'receptid', ',')::int[]) AS rec_id,
-        	UNNEST(string_to_array(:'recept_sorszám', ',')::int[]) AS rec_no,
-		UNNEST(string_to_array(:'összetevő_id', ',')::int[]) AS össz_id,
-		UNNEST(string_to_array(:'mennyiség', ',')::int[]) AS menny,
-		UNNEST(string_to_array(:'mérték_mennyiség_id', ',')::int[]) AS mért_menny_id,
-                UNNEST(string_to array(:'összetevő_osztály_id', ',')::int[]) AS össz_oszt_id,
-		UNNEST(string_to_array(:'recept_osztály_id', ',')::int[]) AS rec_oszt_id
-)      INSERT INTO recept_összetevők (rec_id, rec_no, össz_id, menny, mért_menny_id, össz_oszt_id, rec_oszt_id)
-       SELECT * FROM data;
-                RAISE NOTICE 'A rekordok sikeresen létrejöttek: receptid =%, recept_név =%', $1, $2;
-	END IF;
-	EXCEPTION
-		WHEN OTHER THEN
-			-- Hibakezelés:
-                        RAISE NOTICE 'Hiba történt a beszúrás során. Nem sikerült a művelet!';
-			RAISE EXCEPTION 'Nem várt hiba fordult elő!';
-		END;
-		$$ LANGUAGE plpgsql;
+        SELECT
+            UNNEST(string_to_array(replace(receptid, ' ', ''), ',')::int[]) AS rec_id,
+            UNNEST(string_to_array(replace(recept_sorszám, ' ', ''), ',')::int[]) AS rec_no,
+            UNNEST(string_to_array(replace(összetevő_id, ' ', ''), ',')::int[]) AS össz_id,
+            UNNEST(string_to_array(replace(mennyiség, ' ', ''), ',')::int[]) AS menny,
+            UNNEST(string_to_array(replace(mérték_mennyiség_id, ' ', ''), ',')::int[]) AS mért_menny_id,
+            UNNEST(string_to_array(replace(összetevő_osztály_id, ' ', ''), ',')::int[]) AS össz_oszt_id,
+            UNNEST(string_to_array(replace(recept_osztály_id, ' ', ''), ',')::int[]) AS rec_oszt_id
+    )
+    INSERT INTO recept_összetevők (receptid, recept_sorszám, összetevő_id, mennyiség, mérték_mennyiség_id, összetevő_osztály_id, recept_osztály_id)
+    SELECT rec_id, rec_no, össz_id, menny, mért_menny_id, össz_oszt_id, rec_oszt_id
+    FROM data;
+
+    RAISE NOTICE 'A rekordok sikeresen beszúrásra kerültek!';
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE NOTICE 'Hiba történt! Ellenőrizd az adatokat.';
+        RAISE EXCEPTION 'Nem várt hiba történt a rekordok beszúrása során.';
+END;
+$$ LANGUAGE plpgsql;
+
