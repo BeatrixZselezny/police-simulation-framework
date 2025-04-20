929a58db5608702cecdbea009404e9d677d2145e
Beatrix Zselezny <cattybaby75@gmail.com>
2025-04-20 22:54:49 +0200

    T was tried fix it my uploader.sh file

--- receptek_project/sql_scripts/recept_összetevők_tömeges_upl/uploader.sh
+++ receptek_project/sql_scripts/recept_összetevők_tömeges_upl/uploader.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-csv_file="/police/receptek_project/sql_scripts/recept_összetevők_tömeges_upl/data.csv"
+csv_file="/home/debiana/w3school/objexamples/police/receptek_project/sql_scripts/recept_összetevők_tömeges_upl/data.csv"
 
 db_name="receptek_utf8"
 db_user="postgres"
 log_file="skipped_ids.log"
 echo "" > "$log_file"
 
-   while IFS=, read -r receptid recept_sorszám összetevő_id mennyiség mérték_mennyiség_id összetevő_osztály_idrecept_osztály_id
+   while IFS=, read -r receptid recept_sorszám összetevő_id mennyiség mérték_mennyiség_id összetevő_osztály_id recept_osztály_id
    do
 	   check_result=$(psql -U "$db_user" -d "$db_name" -t -c "SELECT EXISTS(SELECT 1 FROM recept_összetevők WHERE receptid = '$receptid' AND recept_sorszám = '$recept_sorszám');")
 
-	   if [[ "$check_result" =~ t]]; then
+	   if [[ "$check_result" =~ t ]]; then
 		   echo "ReceptID már létezik: $receptid, kihagyva..." | tee -a "$log_file"
 		   continue
 	   fi
