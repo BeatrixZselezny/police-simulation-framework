f563add25f91c18e0aca55edf59c3fa2f502e043
Beatrix Zselezny <cattybaby75@gmail.com>
2025-08-01 23:51:46 +0200

    Remove tracked target files

---
+++ src/main/java/com/yourcompany/police/PoliceSimulationApplication.java
@@ -0,0 +1,11 @@
+package com.yourcompany.police;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliceSimulationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PoliceSimulationApplication.class, args);
+    }
+}
